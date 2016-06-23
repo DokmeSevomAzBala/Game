b4ee1ad8ebea0beca5d0184b2cdf3933f826bba1
niloofarlatifian <niloofar.latifian@gmail.com>
2016-06-23 10:47:13 +0430

    trying to add levels

--- pvz/level.cpp
+++ pvz/level.cpp
@@ -1,7 +1,35 @@
 #include "level.h"
+#include <QDebug>
 
-level::level()
+std::vector<QStringList> levelList;
+int Level::level = 0 ;
+
+Level::Level()
 {
-
 }
 
+void Level::loadLevels()
+{
+    QFile save_file(":/CSVs/pvz_levels.csv");
+    if (save_file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qDebug() <<"LLLLLLLLLLLLLLLLLLLLLLLL";
+        QTextStream text(&save_file);
+        while(!text.atEnd())
+        {
+            levelList.push_back(text.readLine().split(":")); // Fill levelList with info from pvz_levels.csv.
+        }
+    }
+}
+
+bool Level::validLevelFile()
+{
+    QFile save_file(":/CSVs/pvz_levels.csv");
+    if (!save_file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return 0;
+
+    if (!levelList.size())
+        return 0;
+
+    return 1;
+}
