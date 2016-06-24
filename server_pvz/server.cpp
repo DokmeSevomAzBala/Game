d4592e7a3f72b6b9bd742192c96bab265281dd22
elahemohammadzade <e.mohammadzade96@gmail.com>
2016-06-24 12:28:36 -0700

    server added

---
+++ server_pvz/server.cpp
@@ -0,0 +1,57 @@
+#include "server.h"
+
+server::server(QWidget *parent):QWidget (parent)
+{
+    myserver = new QTcpServer(this);
+    if (!myserver->listen(QHostAddress::LocalHost, 5000)) {
+    //ui->log->setPlainText("Failure while starting server: " + myserver->errorString());
+    return;
+    }
+    connect(myserver, SIGNAL(new_connection()),this, SLOT(new_connection()));
+}
+void server::new_connection()
+{
+
+    while (myserver->hasPendingConnections()) {
+    QTcpSocket *con = myserver->nextPendingConnection();
+    myclients << con;
+    //ui->disconnectClients->setEnabled(true);
+    connect(con, SIGNAL(disconnected()), this,
+    SLOT(removeConnection()));
+    connect(con, SIGNAL(readyRead()), this, SLOT(newMessage()));
+    //ui->log->insertPlainText(
+    QString("* New connection: %1, port %2\n")
+    .arg(con->peerAddress().toString())
+    .arg(QString::number(con->peerPort()));
+    }
+
+}
+void server::new_message()
+ {
+     if (QTcpSocket *con = qobject_cast<QTcpSocket*>(sender())) {
+            myreceivedData[con].append(con->readAll());
+            if (!myreceivedData[con].contains(QChar(23)))
+                return;
+
+            QStringList messages = myreceivedData[con].split(QChar(23));
+            myreceivedData[con] = messages.takeLast();
+            foreach (QString message, messages) {
+                //ui->log->insertPlainText("Sending message: " + message + "\n");
+                message.append(QChar(23));
+                foreach (QTcpSocket *socket, myclients) {
+                    if (socket == con)
+                        continue;
+                    if (socket->state() == QAbstractSocket::ConnectedState)
+                        socket->write(message.toLocal8Bit());
+                }
+            }
+        }
+ }
+void server::remove_connection()
+ {
+     if (QTcpSocket *con = qobject_cast<QTcpSocket*>(sender())) {
+             myclients.removeOne(con);
+             qDebug()<<"Disconnected";
+             con->deleteLater();
+         }
+ }
---
+++ server_pvz/server_pvz.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-24T11:16:46
+#
+#-------------------------------------------------
+
+QT       += core gui
+QT       += network
+
+TARGET = server_pvz
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    server.cpp
+
+HEADERS  += mainwindow.h \
+    server.h
+
+FORMS    += mainwindow.ui
