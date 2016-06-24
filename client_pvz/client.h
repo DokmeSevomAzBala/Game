b73a5505b21abe845eed2e0dbbe057dc365b2db0
elahemohammadzade <e.mohammadzade96@gmail.com>
2016-06-24 13:05:50 -0700

    client.header

---
+++ client_pvz/client.h
@@ -0,0 +1,39 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QWidget>
+#include<QTcpSocket>
+#include<QString>
+#include<QTcpServer>
+#include<QGraphicsView>
+#include <QGraphicsScene>
+#include <QMouseEvent>
+
+class client:public QWidget
+{
+    Q_OBJECT
+public:
+    explicit client(QWidget *parent=0);
+    QGraphicsScene *scene;
+    QGraphicsView *view;
+
+    void display_menu();
+    void mouseMoveEvent(QMouseEvent *event);
+
+signals:
+    void start();
+
+private slots:
+    void read_message();
+    void disconnect_by_server();
+
+private:
+      QTcpSocket *mysocket;
+      QString myuser;
+      QString myreceivedData;
+      void updateGui(QAbstractSocket::SocketState state);
+
+
+};
+
+#endif // CLIENT_H
---
+++ client_pvz/client_pvz.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-24T12:32:57
+#
+#-------------------------------------------------
+
+QT       += core gui
+QT       += network
+TARGET = client_pvz
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp \
+    client.cpp
+
+HEADERS  += mainwindow.h \
+    client.h
+
+FORMS    += mainwindow.ui
