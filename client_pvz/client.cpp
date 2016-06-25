09324977b2a6b12fcc54928d7eba2080f508cd65
elahemohammadzade <e.mohammadzade96@gmail.com>
2016-06-24 21:34:24 -0700

    client nesfasa Dg hal nadaram

--- client_pvz/client.cpp
+++ client_pvz/client.cpp
@@ -2,5 +2,8 @@
 
 client::client(QWidget *parent): QWidget(parent)
 {
-
+     mysocket = new QTcpSocket;
+     mysocket->connectToHost("0.0.0.0",5000);
+     qDebug()<<"connctted "<<mysocket->localAddress()<<"  port: "<<5000;
+     view = new QGraphicsView();
 }
